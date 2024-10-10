24c4f26c93d1a7d8962c5b6daf3f853b3fee14ce
Ayman15 <ayman.imbabi@nozom-tech.com>
2024-10-10 17:35:39 +0300

    Add project files.

---
+++ Realtime Plot/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Realtime_Plot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Windows.Forms.Timer plotTimer = new System.Windows.Forms.Timer();
+
+        private void RealTimePlot()
+        {
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
